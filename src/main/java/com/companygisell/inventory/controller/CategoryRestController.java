1f074c7fad324ce91b846aaf3f7cdfdf995dd3d2
Gisell L <lopgiz25@gmail.com>
2025-05-04 13:32:04 +0200

    servicio guardar categoria

--- src/main/java/com/companygisell/inventory/controller/CategoryRestController.java
+++ src/main/java/com/companygisell/inventory/controller/CategoryRestController.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.companygisell.inventory.model.Category;
 import com.companygisell.inventory.response.CategoryResponseRest;
 import com.companygisell.inventory.services.ICategoryService;
 
@@ -37,6 +40,18 @@ public class CategoryRestController {
 	public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id) {
 		
 		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
+		return response;
+	}
+	
+	/**
+	 * save categories
+	 * @param Category
+	 * @return
+	 * */
+	@PostMapping("/categories")
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category) {
+		
+		ResponseEntity<CategoryResponseRest> response = service.save(category);
 		return response;
 	}
 }
--- src/main/java/com/companygisell/inventory/services/ICategoryService.java
+++ src/main/java/com/companygisell/inventory/services/ICategoryService.java
@@ -1,11 +1,13 @@
 package com.companygisell.inventory.services;
 import org.springframework.http.ResponseEntity;
+
+import com.companygisell.inventory.model.Category;
 import com.companygisell.inventory.response.CategoryResponseRest;
 
 public interface ICategoryService {
 	
 	public ResponseEntity<CategoryResponseRest> search();
 	public ResponseEntity<CategoryResponseRest> searchById(Long id);
-
+	public ResponseEntity<CategoryResponseRest> save(Category category);
 
 }
