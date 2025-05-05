22a65037e8da31c634284ebb06ee0df95673542b
Gisell L <lopgiz25@gmail.com>
2025-05-05 16:48:27 +0200

    Configuracion de CORS

--- src/main/java/com/companygisell/inventory/controller/CategoryRestController.java
+++ src/main/java/com/companygisell/inventory/controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.companygisell.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.companygisell.inventory.model.Category;
 import com.companygisell.inventory.response.CategoryResponseRest;
 import com.companygisell.inventory.services.ICategoryService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 public class CategoryRestController {
